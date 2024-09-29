889a96cf30434d2ecf4532bd55556b1f9bd29688
juacevedov <juacevedov@unal.edu.co>
2024-09-29 00:03:52 -0500

    crud de rol y funcionario con bootstrap 3

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -47,35 +47,5 @@
             </tbody>
         </table>
     </div>
-    <h1>Lista de Funcionarios</h1>
-    <a href="{{ route('funcionarios.create') }}">Agregar un Nuevo Funcionario</a>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Usuario</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($funcionarios as $funcionario)
-                <tr>
-                    <td>{{ $funcionario->id_fun }}</td>
-                    <td>{{ $funcionario->nombre_fun }}</td>
-                    <td>{{ $funcionario->user->id }}</td>
-                    <td>
-                        <a href="{{ route('funcionarios.show', $funcionario->id_fun) }}">Ver</a>
-                        <a href="{{ route('funcionarios.edit', $funcionario->id_fun) }}">Editar</a>
-                        <form action="{{ route('funcionarios.destroy', $funcionario->id_fun) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Eliminar</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
 </body>
 </html>

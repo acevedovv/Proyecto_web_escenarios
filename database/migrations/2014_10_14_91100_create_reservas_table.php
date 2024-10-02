63b744feb905e5aefd7b055422e4dbefe5ee8e28
juacevedov <juacevedov@unal.edu.co>
2024-10-02 00:03:47 -0500

    se agrega funcion de formulario

--- database/migrations/2014_10_14_91100_create_reservas_table.php
+++ database/migrations/2014_10_14_91100_create_reservas_table.php
@@ -13,22 +13,22 @@ class CreateReservasTable extends Migration
      */
     public function up()
     {
-        #Schema::dropIfExists('reservas');
         Schema::create('reservas', function (Blueprint $table) {
             $table->id('id_res');
-            $table->date('fecha_res');
-            $table->date('fecha_dev');
+            $table->dateTime('fecha_res'); // Cambiado a dateTime para incluir la hora
+            $table->dateTime('fecha_dev'); // Cambiado a dateTime para incluir la hora de fin
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('id_esc');
             $table->timestamps();
             
             // Definir la relación con la tabla usuarios
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
+    
             // Definir la relación con la tabla escenarios_deportivos
             $table->foreign('id_esc')->references('id_esc')->on('escenarios_deportivos')->onDelete('cascade');
         });
     }
+    
 
 
     /**
